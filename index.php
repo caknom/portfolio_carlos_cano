79f21b698aa526a7dccda1e402a5efb4415ba619
Carlos Cano <caknom@gmail.com>
2025-01-15 13:04:03 -0500

    Resume Webpage Update
    In this commit, I am merging the changes I have made on vacations

--- index.php
+++ index.php
@@ -7,106 +7,110 @@ $results = mysqli_query($connect,$query);
 ?>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="initial-scale=1.0, width=device-width">
-    <link href="css/grid.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
-    <script defer src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script>
-    <script defer src="js/ScrollSmoother.min.js"></script>
-    <script defer src="js/main.js"></script>
-    <title>Carlos Cano Portfolio</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="initial-scale=1.0, width=device-width">
+  <link href="css/grid.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+  <script defer src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
+  <script defer src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script>
+  <script defer src="js/ScrollSmoother.min.js"></script>
+  <script defer src="js/main.js"></script>
+  <title>Carlos Cano Portfolio</title>
 </head>
 
 <div id="smooth-wrapper">
   <div id="smooth-content">
     
-<body>
-
-      <!-- Main Nav -->
-      <h1 class="hidden">Carlos Cano Portfolio</h1>
-      <div id="sticky-nav-con">
-          <header class="grid-con">
-            <div id="logo" class="col-span-2 m-col-start-1 m-col-span-2">
-                <img src="images/icon-logo.svg" alt="Ca Web Developer Logo">
-            </div>
-            <nav id="main-nav" class="col-start-4 m-col-start-5 m-col-end-13 l-col-start-4 l-col-end-13">
-              <h2 class="hidden">Main Navigation</h2>
-              <button id="button"></button>
-              <div id="burger-con">
-                <ul>
-                  <li><a href="#work-section">Works</a></li>
-                  <li><a href="#ref-main">References</a></li>
-                  <li><a href="#contact-main">Contact</a></li>
-                </ul>
-              </div>
-            </nav>
-          </header>
-      </div>
+    <body>
+      <section class="app">
+        <h1 class="hidden">Carlos Cano Portfolio</h1>
+        <div id="sticky-nav-con" class="nav">
+            <header class="grid-con">
+              <nav id="main-nav" class="col-span-full">
+                <h2 class="hidden">Main Navigation</h2>
+                <button id="nav-works">Study Cases</button>
+                <button id="nav-references">References</button>
+                <button id="nav-contact">Contact</button>
+              </nav>
+            </header>
+        </div>
+      </section>
 
       <main>
-
-        <section id="hero-section" class="grid-con">
-          <h2 class="hidden">Hero Section</h2>
-          <div id="logo-icon-ca" class="col-span-full m-col-start-2 m-col-end-5">
-            <img src="images/icon-logo-ca.svg">
+        
+        <section id="hero-section" class="col-span-full" data-speed="1">
+          <h1 id="name-main">CARLOS</h1>
+          <h1 id="lname-main" data-speed="1.2">CANO</h1>
+          <div class="slider-con">
+            <div class="slider" data-speed="1.5">
+              <p>Front-End Web Developer - Graphic & Motion Designer - Photographer & Videographer -</p>
+              <p>Front-End Web Developer - Graphic & Motion Designer - Photographer & Videographer -</p>
+            </div>
           </div>
-          <div class="col-span-full m-col-start-5 m-col-end-11">
-            <p id="hero-text-1">Hi, I’m Carlos, a web developer living in Canada. The "CA" in my portfolio name stands for both my initials and Canada, where I’m currently crafting websites — a fun little coincidence!</p>
-            <p id="hero-text-2">Coming from a graphic design background, I’ve shifted my focus to front-end development, creating dynamic and visually appealing websites.</p>
-          </div>
-          <img src="images/arrow-down.svg" class="arrow-down col-span-full m-col-span-1">
         </section>
 
+        <section class="grid-con">
+          <div class="hero-text-con col-span-full">
+            <div class="hero-text-con-1 m-col-start-3 m-col-end-6">
+              <p>Hi, I’m Carlos, a web developer living in Canada. The "CA" in my portfolio name stands for both my initials and Canada, where I’m currently crafting websites — a fun little coincidence!</p>
+            </div>
+            <div class="hero-text-con-2" >
+              <p>Coming from a graphic design background, I’ve shifted my focus to front-end development, creating dynamic and visually appealing websites.</p>
+            </div>
+          </div>
+          <img src="images/arrow-down.svg" class="arrow-down col-span-full m-col-span-1" data-lag="1.3">
+        </section>
+        
 
-        <section id="work-section" class="col-span-full">
+
+        <section id="work-section" class="col-span-full skew">
           <h2 class="hidden">Works Section</h2>
           <h2 class="titles-main col-span-full">WORKS</h2>
           <div class="col-span-full">
             <video src="videos/main_reel.mp4" playsinline loop muted autoplay></video>
           </div>
-          <h2 class="titles-main col-span-full">STUDY CASES</h2>
+          <h2 id="studyCases-main" class="titles-main col-span-full">STUDY CASES</h2>
 
-        <?php
-    while($row = mysqli_fetch_assoc($results)) {
-    echo ' 
-    
-          <div id="projects-section" class="grid-con">
-            <h2 class="main-work-title col-span-full"> '.$row["title"].' - <br>Webpage</h2>
-            <h5 class="col-span-full">'.$row["date"].'</h5>
-            <img src="images/arrow-left-right.svg" class="arrow-left-right col-span-full">
-            <div class="col-span-full">
-              <video src="videos/'.$row["video"].'" playsinline loop muted autoplay></video>
-            </div>
-          </div>
-
-    
-          <div class="grid-con">
-            <div class="text-goal col-span-full m-col-span-3">
-              <h3>Goal</h3>
-              <p>'.$row["goal"].'</p>
-            </div>
-            <div class="text-project col-span-full m-col-span-8">
-              <div>
-                <h3>Process</h3>
-                <p>'.$row["process"].'</p>
-              </div>
-              <div>
-                <h3>Outcome</h3>
-                <p>'.$row["outcome"].'</p>
+          <?php
+          while($row = mysqli_fetch_assoc($results)) {
+          echo ' 
+          
+            <div id="projects-section" class="grid-con">
+              <h2 class="main-work-title col-span-full"> '.$row["title"].' - <br>Webpage</h2>
+              <h5 class="col-span-full">'.$row["date"].'</h5>
+              <img src="images/arrow-left-right.svg" class="arrow-left-right col-span-full">
+              <div class="col-span-full">
+                <video src="videos/'.$row["video"].'" playsinline loop muted autoplay></video>
               </div>
             </div>
-            <img src="images/arrow-down-white.svg" class="arrow-down col-span-full m-col-start-12">
-          </div>
-    
-    '
-    ;
-    }
-    ?>
-    </section>
+
+          
+            <div id="goal-bg" class="grid-con">
+              <div class="text-goal col-span-full m-col-span-3" data-lag="0.3">
+                <h3>Goal</h3>
+                <p>'.$row["goal"].'</p>
+              </div>
+              <div class="text-project col-span-full m-col-span-8">
+                <div data-lag="0.2">
+                  <h3>Process</h3>
+                  <p>'.$row["process"].'</p>
+                </div>
+                <div data-lag="0.4">
+                  <h3>Outcome</h3>
+                  <p>'.$row["outcome"].'</p>
+                </div>
+              </div>
+              <img src="images/arrow-down-white.svg" class="arrow-down col-span-full m-col-start-12" data-lag="1">
+            </div>
+          
+          '
+          ;
+          }
+          ?>
+        </section>
 
 
-        <section id="ref-main">
+        <section id="ref-main" data-speed="0.7">
           <h2 class="titles-main col-span-full">REFERENCES</h2>
           <div class="grid-con">
             <h2 class="hidden">References Section</h2>
@@ -138,30 +142,28 @@ $results = mysqli_query($connect,$query);
           </div>
         </section>
 
-        <section id="contact-main" class="grid-con">
+        <section id="contact-main" data-speed="1.2">
           <h2 class="titles-main col-span-full">CONTACT</h2>
+          <div class="grid-con">
+            <h2 class="col-span-full">Share your brillant idea!</h2>
+            <div id="contact-hero-form" class="col-span-full m-col-start-1 m-col-end-8">
+              <form action="#" method="post" enctype="text/plain">
+                  <input name="fullName" type="text" required placeholder="Name">
+                  <input name="contactInfo" type="email" required placeholder="Email">
+                  <textarea name="msg" placeholder="Message"></textarea>
+                  <input name="submit" type="submit" value="Send me a message">
+              </form>
+            </div>
 
-          <h2 class="col-span-full">Share your brillant idea!</h2>
-          <div id="contact-hero-form" class="col-span-full m-col-start-1 m-col-end-8">
-            <form action="#" method="post" enctype="text/plain">
-                <input name="fullName" type="text" required placeholder="Name">
-                <input name="contactInfo" type="email" required placeholder="Email">
-                <textarea name="msg" placeholder="Message"></textarea>
-                <input name="submit" type="submit" value="Send me a message">
-            </form>
-          </div>
-
-          <div id="contact-text" class="col-span-full m-col-start-8 m-col-end-13">
-            <p>Here you can check more details of my background<br>
-            <a href="https://www.linkedin.com/in/carlos-cano-m/" target="_blank">LinkedIn</a></p>
-            <p>and here you can send me a direct email<br>
-            <a href="mailto:hello@carloscano.ca" target="_blank">hello@carloscano.ca</a></p>
+            <div id="contact-text" class="col-span-full m-col-start-8 m-col-end-13">
+              <p>Here you can check more details of my background<br>
+              <a href="https://www.linkedin.com/in/carlos-cano-m/" target="_blank">LinkedIn</a></p>
+              <p>and here you can send me a direct email<br>
+              <a href="mailto:hello@carloscano.ca" target="_blank">hello@carloscano.ca</a></p>
+            </div>
           </div>
         </section>
       </main>
-      <footer class="grid-con">
-      </footer>
-
     </body>
     
     <footer id="footer-main" class="col-span-full">
--- js/main.js
+++ js/main.js
@@ -1,29 +1,46 @@
 (() => {
     gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-let smoother = ScrollSmoother.create({
-    wrapper: '#smooth-wrapper',
-    content: '#smooth-content',
-    smooth: 1.5,
-    smoothTouch: 0.1
-})
-})();
+    let smoother = ScrollSmoother.create({
+        wrapper: '#smooth-wrapper',
+        content: '#smooth-content',
+        smooth: 1.5,
+        smoothTouch: 0.1,
+        effects: true
+    })
 
-(() => {
-    let burger = document.querySelector("#button");
-    let burgerCon = document.querySelector("#burger-con");
+    let nav_works = document.querySelector('#nav-works');
+    let nav_references = document.querySelector('#nav-references');
+    let nav_contact = document.querySelector('#nav-contact');
 
-    burger.addEventListener("click", () => {
-        burger.classList.toggle("expanded");
-        burgerCon.classList.toggle("slide-toggle");
+    nav_works.addEventListener('click', (e) => {
+        smoother.scrollTo('#studyCases-main', true, 'top top')
     });
-})();
+    nav_references.addEventListener('click', (e) => {
+        smoother.scrollTo('#ref-main', true, 'top top')
+    });
+    nav_contact.addEventListener('click', (e) => {
+        smoother.scrollTo('#contact-main', true, 'top top')
+    });
 
-(() => {
-    document.addEventListener("DOMContentLoaded", () => {
-        gsap.from("#hero-text-1", { opacity: 0, y: 50, duration: 1 });
-        gsap.from("#hero-text-2", { opacity: 0, y: 50, duration: 1, delay: 0.3 });
-        gsap.from(".arrow-down", { opacity: 0, y: 20, duration: 1, delay: 1.2 });
+    let tween = gsap.to(".slider", {
+        xPercent: -100,
+        repeat: -1,
+        duration: 10,
+        ease: "linear"
+    });
+
+
+    let skewSetter = gsap.quickTo('.skew', 'skewY');
+    let clamp = gsap.utils.clamp(-2, 2);
+
+    ScrollSmoother.create({
+        wrapper: '#wrapper',
+        content: '#content',
+        smooth: 2,
+        effects: true,
+        onUpdate: self => skewSetter(clamp(self.getVelocity() / -100)),
+        onStop: () => skewSetter(0)
     });
 })();
 
@@ -36,4 +53,30 @@ let smoother = ScrollSmoother.create({
             gsap.to(element, { scale: 1, duration: 0.3, ease: "power1.in" });
         });
     });
+})();
+
+
+(() => {
+
+    let menuStatic = ScrollTrigger.create({
+        trigger: "#sticky-nav-con",
+        pin: "#main-nav",
+        start: "top top",
+        end: "+=10000",
+    });
+
+    let st1 = ScrollTrigger.create({
+        trigger: ".hero-text-con",
+        pin: ".hero-text-con-1",
+        start: "top top",
+        end: "+=2000",
+    });
+
+    let st2 = ScrollTrigger.create({
+        trigger: ".hero-text-con",
+        pin: ".hero-text-con-2",
+        start: "top top",
+        end: "+=2000",
+    });
+
 })();
